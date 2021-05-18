6f1692ab97204fac46f7bdbdf71f286453a73348
Patrick Hart <patrick.hart@centare.com>
2021-05-18 16:58:48 -0500

    Test two refactor

--- Kata/tests/FizzBuzzExample.Tests/FIzzBuzzTDD.cs
+++ Kata/tests/FizzBuzzExample.Tests/FIzzBuzzTDD.cs
@@ -11,11 +11,16 @@ namespace FizzBuzzExample.Tests
         //For a set of numbers, 1 through N
         //If n is divisible by 3, print Fizz
         //If n is divisible by 5, print Buzz
+        private readonly FizzBuzzTddActual fizzBuzz;
+
+        public FizzBuzzTDD()
+        {
+            fizzBuzz = new FizzBuzzTddActual();
+        }
+
         [Fact]
         public void when_n_is_1_result_should_be()
         {
-            var fizzBuzz = new FizzBuzzTddActual();
-
             var result = fizzBuzz.Test(1);
 
             result.Should().BeEmpty();
@@ -24,8 +29,6 @@ namespace FizzBuzzExample.Tests
         [Fact]
         public void when_n_is_3_result_should_be()
         {
-            var fizzBuzz = new FizzBuzzTddActual();
-
             var result = fizzBuzz.Test(3);
 
             result.Should().Be("Fizz");
