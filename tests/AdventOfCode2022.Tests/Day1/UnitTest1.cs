c73d177d5bd7b1a778de439fa783bd9fbf842051
Patrick <devlear@Gmail.com>
2022-12-03 13:25:00 -0600

    update packages, .net version, add initial testing project

---
+++ tests/AdventOfCode2022.Tests/Day1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using AutoFixture;
+
+namespace AdventOfCode2022
+{
+    public class UnitTest1
+    {
+        public readonly Fixture _fixture;
+
+        public UnitTest1()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public void when_adding_food_calorie_total_should_be()
+        {
+
+        }
+    }
+}
