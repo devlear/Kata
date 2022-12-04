8ab533dc37fecb1401376931b5328da3fb417df0
Patrick <Hart>
2022-12-03 20:08:13 -0600

    Put counting code into new class

---
+++ src/AdventOfCode2022/Day1/FoodCounter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day1
+{
+    public static class FoodCounter
+    {
+        public static IEnumerable<Elf> CountFood(string path)
+        {
+            var elfList = new List<Elf>();
+            using (var reader = new StreamReader(path))
+            {
+                var foodReader = new FoodReader(reader);
+                int[] foodValues;
+                while ((foodValues = foodReader.Next()).Length != 0)
+                {
+                    var food = foodValues.Select(calorie => new Food { Calories = calorie })
+                        .ToArray();
+                    var elf = Elf.Create();
+                    Array.ForEach(food, elf.AddFood);
+                    elfList.Add(elf);
+                }
+            }
+            return elfList.AsEnumerable();
+        }
+
+        public static int CountCaloriesForTopX(this IEnumerable<Elf> elves, int topX)
+        {
+            var sortedElves = elves.OrderByDescending(elf => elf.TotalCalories);
+            return sortedElves.Take(topX)
+                .First()
+                .TotalCalories;
+        }
+    }
+}
---
+++ src/AdventOfCode2022/Program.cs
@@ -0,0 +1,20 @@
+﻿//Count all food on Elfs - done
+//create a way to part file / string into int array - done
+//Create elf and add food while parsing list
+//Get elf with most TotalCalories
+
+// See https://aka.ms/new-console-template for more information
+
+
+using AdventOfCode2022.Day1;
+
+var path = args[0];
+
+var elfList = FoodCounter.CountFood(path);
+var maxCalorie = elfList.CountCaloriesForTopX(1);
+Console.WriteLine($"Biggest Calorie count is: {maxCalorie}");
+
+var topX = 3;
+var topXTotal = elfList.CountCaloriesForTopX(topX);
+
+Console.WriteLine($"Top {topX} count is: {topXTotal}");
